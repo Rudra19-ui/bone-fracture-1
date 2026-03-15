113b4e374ad68d2f5aa3ae831e7acc0b6ab843fc
Rudra19-ui <rudrajosi19072004@gmail.com>
2026-03-15 21:37:35 +0530

    Final cleanup: Removed virtual environments and junk binaries. Ready for GitHub push.

--- backend/fracture/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class FractureConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fracture'
--- backend/fracture/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import AnalysisView
-
-urlpatterns = [
-    path('analysis', AnalysisView.as_view()),
-]
